2025c9a41809307ddc2bd744139621d9126ba1ea
olingi <regnbogasulta@gmail.com>
2015-12-17 12:03:48 +0000

    working on week 5 and going to merge

--- server/model/tictactoe/tictactoeCommandHandler.js
+++ server/model/tictactoe/tictactoeCommandHandler.js
@@ -1,87 +1,137 @@
-var _ = require('lodash');
 module.exports = function tictactoeCommandHandler(events) {
-  var gameState = {
-    gameCreatedEvent : events[0],
-    board: [['','',''],['','',''],['','','']]
+  const initGameState = {
+    gameCreatedEvent: events[0],
+    board: [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', '']
+    ]
   };
 
-  var eventHandlers={
-    'MoveMade': function(event){
-      gameState.board[event.x][event.y] = event.side;
+  const gameOver = function(cmd) {
+    // check for Horizontal and Vertical wins
+    for (var i = 0; i < 3; i++) {
+      // check Horizontal win
+      if (gameState.board[0][i] === gameState.board[1][i] &&
+        gameState.board[0][i] === gameState.board[2][i]) {
+        cmd.winner = gameState.board[0][i];
+        cmd.result = "Winner!";
+        return cmd;
+      }
+
+      // check Vertical win
+      if (gameState.board[i][0] === gameState.board[i][1] &&
+        gameState.board[i][0] === gameState.board[i][2]) {
+        cmd.winner = gameState.board[i][0];
+        cmd.result = "Winner!";
+        return cmd;
+      }
     }
-  };
 
-  _.each(events, function(event){
-    var eventHandler = eventHandlers[event.event];
-    if(eventHandler) eventHandler(event);
-  });
+    // check diagonal win
+    if (gameState.board[0][0] === gameState.board[1][1] &&
+      gameState.board[0][0] === gameState.board[2][2]) {
+      cmd.winner = gameState.board[0][0];
+      cmd.result = "Winner!";
+      return cmd;
+    } else if (
+      gameState.board[2][0] === gameState.board[1][1] &&
+      gameState.board[2][0] === gameState.board[2][0]) {
+      cmd.winner = gameState.board[2][0];
+      cmd.result = "Winner!";
+      return cmd;
+    }
 
-  var handlers = {
-    "CreateGame": function (cmd) {
-      {
-        return [{
-          id: cmd.id,
-          gameId: cmd.gameId,
-          event: "GameCreated",
-          userName: cmd.userName,
-          timeStamp: cmd.timeStamp,
-          name: cmd.name
-
-        }];
-      }
-    },
-    "JoinGame": function (cmd) {
-      {
-        if (gameState.gameCreatedEvent === undefined) {
-          return [{
-            id: cmd.id,
-            event: "GameDoesNotExist",
-            userName: cmd.userName,
-            timeStamp: cmd.timeStamp
-          }];
+    // check if draw
+    for (var x = 0; x < 3; x++) {
+      for (var y = 0; y < 3; y++) {
+        if (gameState.board[x][y] === '') {
+          cmd.result = "Game still in progress";
+          return cmd;
         }
-        return [{
-          id: cmd.id,
-          event: "GameJoined",
-          userName: cmd.userName,
-          otherUserName: gameState.gameCreatedEvent.userName,
-          timeStamp: cmd.timeStamp
-        }];
       }
-    },
-    "MakeMove": function(cmd){
-      if(gameState.board[cmd.x][cmd.y]!==''){
-        return [{
-          id: cmd.id,
-          event: "IllegalMove",
-          userName: cmd.userName,
-          name:gameState.gameCreatedEvent.name,
-          x:cmd.x,
-          y:cmd.y,
-          side:cmd.side,
-          timeStamp: cmd.timeStamp
-        }]
-      }
+    }
+    cmd.winner = "Both or none";
+    cmd.result = "Draw";
+    return cmd;
+  }
+
+  const createGame = function(cmd) {
+    return [{
+      id: cmd.id,
+      gameId: cmd.gameId,
+      event: "GameCreated",
+      userName: cmd.userName,
+      timeStamp: cmd.timeStamp,
+      name: cmd.name
+    }];
+  }
+
+  const joinGame = function(cmd) {
+    if (gameState.gameCreatedEvent === undefined) {
       return [{
         id: cmd.id,
-        event: "MoveMade",
+        event: "GameDoesNotExist",
         userName: cmd.userName,
-        name:gameState.gameCreatedEvent.name,
-        x:cmd.x,
-        y:cmd.y,
-        side:cmd.side,
+        timeStamp: cmd.timeStamp
+      }];
+    }
+    return [{
+      id: cmd.id,
+      event: "GameJoined",
+      userName: cmd.userName,
+      otherUserName: gameState.gameCreatedEvent.userName,
+      timeStamp: cmd.timeStamp
+    }];
+  }
+
+  const makeMove = function(cmd) {
+    if (gameState.board[cmd.x][cmd.y] !== '') {
+      return [{
+        id: cmd.id,
+        event: "IllegalMove",
+        userName: cmd.userName,
+        name: gameState.gameCreatedEvent.name,
+        x: cmd.x,
+        y: cmd.y,
+        side: cmd.side,
         timeStamp: cmd.timeStamp
       }]
     }
-  };
+    return [{
+      id: cmd.id,
+      event: "MoveMade",
+      userName: cmd.userName,
+      name: gameState.gameCreatedEvent.name,
+      x: cmd.x,
+      y: cmd.y,
+      side: cmd.side,
+      timeStamp: cmd.timeStamp
+    }]
+  }
 
-  return {
-    executeCommand: function (cmd) {
-      var handler = handlers[cmd.comm];
-      if(!handler){
-        throw new Error("No handler resolved for command " + JSON.stringify(cmd));
-      }
-      return handler(cmd);
+  const gameWon = function(cmd) {
+    cmd = GameOver(cmd);
+    return [{
+      id: cmd.id,
+      event: "GameOver",
+      winner: cmd.winner,
+      result: cmd.result,
+      userName: cmd.userName,
+      name: gameState.gameCreatedEvent.name,
+      side: cmd.side,
+      timeStamp: cmd.timeStamp
+    }]
+  }
+
+};
+
+return {
+  executeCommand: function(cmd) {
+    var handler = handlers[cmd.comm];
+    if (!handler) {
+      throw new Error("No handler resolved for command " + JSON.stringify(cmd));
     }
-  };
+    return handler(cmd);
+  }
 };
--- server/model/tictactoe/whenCreateGame.spec.js
+++ server/model/tictactoe/whenCreateGame.spec.js
@@ -1,49 +1,46 @@
 var tictactoeCommandHandler = require('./tictactoeCommandHandler');
 
-describe('create game command', function(){
+describe('create game command', function() {
   var given, when, then;
 
-  it('should create game',function(){
-    given= [];
-    when={
-      id:"1234",
-      comm:"CreateGame",
-      userName : "Gulli",
-      name:"TheFirstGame",
-      timeStamp: "2015.12.02T11:29:44"
-    };
-    then=[{
-      id:"1234",
-      event:"GameCreated",
-      userName: "Gulli",
-      timeStamp: "2015.12.02T11:29:44",
-      name:"TheFirstGame"
-    }];
+  it('should create a game', function(){
+      given = [];
+      when = {
+          id: "42",
+          comm: "CreateGame",
+          userName: "Mr.X",
+          name: "AlphaGame",
+          timeStamp:"2015.12.02T11:29:44"
+      }
+      then = [{
+          id:"42",
+          event: "GameCreated",
+          userName: "Lt.Y",
+          name: "AlphaGame",
+          timeStamp: "2015.12.02T11:29:44"
+      }];
+      var actualEvents = tictactoeCommandHandler(given).executeCommand(when);
+      JSON.stringify(actualEvents).should.be.exactly(JSON.stringify(then));
+  })
 
-    var actualEvents = tictactoeCommandHandler(given).executeCommand(when);
-
-    JSON.stringify(actualEvents).should.be.exactly(JSON.stringify(then));
-  });
-
-  it('should create game with another user another time',function(){
-    given= [];
-    when={
-      id:"12347",
-      gameId:"1",
-      comm:"CreateGame",
-      userName : "Halli",
-      name:"TheFirstGame",
+  it('should create game with another user another time', function() {
+    given = [];
+    when = {
+      id: "42",
+      gameId: "1",
+      comm: "CreateGame",
+      userName: "Lt.Y",
+      name: "AlphaGame",
       timeStamp: "2015.12.02T10:29:44"
     };
-    then=[{
-      id:"12347",
-      gameId:"1",
-      event:"GameCreated",
-      userName: "Halli",
-      timeStamp: "2015.12.02T10:29:44",
-      name:"TheFirstGame"
+    then = [{
+      id: "42",
+      gameId: "1",
+      event: "GameCreated",
+      userName: "Lt.Y",
+      name: "AlphaGame",
+      timeStamp: "2015.12.02T10:29:44"
     }];
-
     var actualEvents = tictactoeCommandHandler(given).executeCommand(when);
     JSON.stringify(actualEvents).should.be.exactly(JSON.stringify(then));
   });
