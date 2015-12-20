53715add90dd5f9ca03beab56b0489d4f4e4a658
olingi <regnbogasulta@gmail.com>
2015-12-20 16:06:01 +0000

    fluid test not working because strange error message

--- server/fluid-api/tictactoeFluid.js
+++ server/fluid-api/tictactoeFluid.js
@@ -1,50 +1,57 @@
 var should = require('should');
 var request = require('supertest');
+var async = require('async');
 var acceptanceUrl = process.env.ACCEPTANCE_URL;
 
+var getUri = function(cmd){
+    if (cmd.comm === "createGame")
+        return "/api/createGame";
+    else if (cmd.comm === "makeMove")
+        return "/api/makeMove";
+    else if (cmd.comm === "joinGame")
+        return "/api/joinGame";
+};
+
 function given(userApi) {
-  var _expectedEvents=[{
-    "id": "42",
-    "gameId": userApi._command.gameId,
-    "event": "EventName",
-    "userName": userApi._command.userName,
-    "name": userApi._command.gameId,
-    "timeStamp": "2014-12-02T11:29:29"
-  }];
-  var _currentEvent = 0;
+  var commands = [userApi._command];
+  var currentEvent = [];
   var expectApi = {
     withName: function (gameName) {
-      _expectedEvents[_currentEvent].name = gameName;
+      currentEvent[currentEvent.length - 1].name = gameName;
       return expectApi;
     },
     expect: function (eventName) {
-      _expectedEvents[_currentEvent].event = eventName;
+      currentEvent.push({event: eventName});
       return expectApi;
     },
     isOk: function (done) {
-      var req = request(acceptanceUrl);
-      req
-        .post('/api/createGame')
-        .type('json')
-        .send(userApi._command)
-        .end(function (err, res) {
-          if (err) return done(err);
-          request(acceptanceUrl)
-            .get('/api/gameHistory/' + userApi._command.gameId)
+        async.eachSeries(commands, function (cmd, callback) {
+            request(acceptanceUrl)
+            .post(getUri(cmd))
+            .type('json')
+            .send(cmd)
+            .end(function(err, res){
+                if (err) {
+                    return done(err);
+                }
+                callback();
+            });
+        },function(err){
+            request(acceptanceUrl)
+            .get('/api/gameHistory' + userApi.gameId)
             .expect(200)
-            .expect('Content-Type', /json/)
+            .expect('Content-Type', '/json/')
             .end(function (err, res) {
-              if (err) return done(err);
-              res.body.should.be.instanceof(Array);
-              should(res.body).eql(
-                _expectedEvents);
-              done();
+                if (err){
+                    return done(err);
+                }
+                res.body.should.be.instanceof(Array);
+                should(res.body[res.body.length - 1]).match(currentEvent[currentEvent.length - 1]);
+                done();
             });
         });
-      return expectApi;
-    },
-  };
-
+    }
+    };
   return expectApi;
 }
 
@@ -57,7 +64,8 @@ function user(userName) {
         gameId: gameId,
         comm: "CreateGame",
         userName: userName,
-        name: gameId,
+        side: "X",
+        name: "AlphaGame",
         timeStamp: "2014-12-02T11:29:29"
       };
       return userApi;
